ed178d54a2ddcd5493e39b67edb41c24b325eaa7
yl <yl@microsoft.com>
2019-10-30 13:45:59 -0700

    Add spinning cube logic to BasicXrApp

--- samples/BasicXrApp/App.h
+++ samples/BasicXrApp/App.h
@@ -19,8 +19,10 @@
 namespace sample {
     struct Cube {
         xr::SpaceHandle Space;
-        XrPosef Pose = xr::math::Pose::Identity();
+        std::optional<XrPosef> PoseInSpace = {}; // Cube pose in above Space. Default to identity.
         XrVector3f Scale{0.1f, 0.1f, 0.1f};
+
+        XrPosef PoseInScene = xr::math::Pose::Identity(); // Cube pose in the scene.  Got updated every frame
     };
 
     struct IOpenXrProgram {
