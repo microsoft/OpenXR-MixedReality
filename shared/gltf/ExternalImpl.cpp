149990c9182add338abbc49aab08c1e07c1ff27f
Matt Jaeger <majaeger@microsoft.com>
2020-07-23 14:38:56 -0700

    Update lib names

--- shared/gltf/ExternalImpl.cpp
+++ shared/gltf/ExternalImpl.cpp
@@ -7,7 +7,6 @@
 #define TINYGLTF_USE_RAPIDJSON_CRTALLOCATOR
 #define TINYGLTF_NO_STB_IMAGE_WRITE
 #define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
 #include <tiny_gltf.h>
 
 #include <mikktspace.cpp>
