a2e2db9bab9e1d9ae383a80dcc2a9b3892288f4c
lilseyi <40449263+lilseyi@users.noreply.github.com>
2020-09-16 16:10:13 -0400

    Import necessary BGFX libraries Trial 2 (#63)
    * Update samples project to consume bgfx via submodules and wil via nuget
    
    Co-authored-by: Matt Jaeger <majaeger@microsoft.com>
    Co-authored-by: James Dooley <jdooley@microsoft.com>

---
+++ Generate-BgfxProjects.ps1
@@ -0,0 +1,10 @@
+& git submodule update --init
+
+try {
+    Push-Location $PSScriptRoot\shared\ext\bgfx
+    & ..\bin\windows\genie --with-windows=10.0 --with-dynamic-runtime --vs=winstore100 vs2019
+    & ..\bin\windows\genie --with-windows=10.0 --with-dynamic-runtime vs2019
+}
+finally {
+    Pop-Location
+}
